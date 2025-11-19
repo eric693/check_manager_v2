f0b6ad49ed660256b93d499e2380130e42831ddc
eric693 <exo881222@gmail.com>
2025-11-19 23:43:52 +0800

    update

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbwQeSXcQUO4zgazZb9yxq6EvA1mcmtZTWpuv-LWTApIirGOG_7OGTIYx3beLjGNKz8fgQ/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxpu8CcMNxMD-C5IclcRN8OsE-jDMcVBQwh_G1p3pm2lw5YMT2W9AHk3mtPO0Lo6Wdh/exec",
   
   // 新增回呼網址
-  redirectUrl: "https://eric693.github.io/New_Attendance-System/"
+  redirectUrl: "https://eric693.github.io/jiman/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.2.2'
